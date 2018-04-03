0b6a53066587f4621f146f91680de9326b1a1b46
jojelupipa <jesus_38b@hotmail.com>
2018-04-03 19:13:58 +0200

    P2: Attempt to add various flying objects and moving them

--- practica_2/robot/objetoVolador.js
+++ practica_2/robot/objetoVolador.js
@@ -7,8 +7,24 @@ class ObjetoVolador extends THREE.Object3D {
 	this.radius = 5
 	this.spawnXpos = 50
 	this.spawnYPos = 40
-	this.spawnZPos = 0
+	this.spawnZPos = Math.floor(Math.random() * Math.floor(200)) -
+	100 // Select number between -100,100
 	
     }
 
+    moveTowardsNegativeZ() {
+	this.sphere.position.z -= 1
+	if (this.sphere.position.z < -100)
+	    this.sphere.position.z = 100
+    }
+
+    moveTowardsNegativeX() {
+	this.sphere.position.x -= 1
+	if (this.sphere.position.x < -100){
+	    this.sphere.position.x = 100
+	    var newZ = Math.floor(Math.random() * Math.floor(200)) -
+	100 // Select number between -100,100 
+		this.sphere.position.z = newZ
+	}
+    }
 }
