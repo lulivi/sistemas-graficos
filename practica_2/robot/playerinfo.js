239dcc1b21b45a0876e67a29f6feeebdcc7294c2
lulivi <luislivilla@gmail.com>
2018-04-17 02:43:25 +0200

    P2: Added energy changes feedback in GUI

--- practica_2/robot/playerinfo.js
+++ practica_2/robot/playerinfo.js
@@ -8,38 +8,57 @@ var PlayerInfo = function() {
     container.style.cssText = 'height: 130px; font-size: 100%;' +
         'background-color: #FFE1F0; border-width: 1%; border-style:' +
         'solid;border-color: #FF96CA; display: inline-block; padding:' +
-        '10px'; 
+        '10px';
 
     // Player Score
     var playerScoreDiv = document.createElement('div');
     playerScoreDiv.id = 'playerScoreDiv';
-    playerScoreDiv.style.cssText = 'height: 20%; width: 80px;';
+    playerScoreDiv.style.cssText = 'height: 20%; width: 50px;';
     container.appendChild(playerScoreDiv);
 
     var playerScoreValue = document.createElement('div');
     playerScoreValue.id = 'playerScoreValue';
     playerScoreValue.style.cssText = 'font-family: Monospace;' +
-        'display: inline-block'; 
+        'display: inline-block';
     playerScoreDiv.appendChild(playerScoreValue);
 
     var playerScoreText = document.createElement('div');
     playerScoreText.id = 'playerScoreText';
     playerScoreText.style.cssText = 'font-size: 70%; padding-left:' +
-    '3%; display: inline-block; margin-bottom: 0px; padding-bottom: 0px;'; 
-    playerScoreText.innerHTML = 'puntos';
+    '3%; display: inline-block; margin-bottom: 0px; padding-bottom: 0px;';
+    playerScoreText.innerHTML = 'points';
     playerScoreDiv.appendChild(playerScoreText);
+
+    // Energy text
+    var playerEnergyTextDiv = document.createElement('div');
+    playerEnergyTextDiv.id = 'playerEnergyTextDiv';
+    playerEnergyTextDiv.style.cssText ='height: 20%; top: 20%; width: 80px';
+    container.appendChild(playerEnergyTextDiv);
+
+    var playerEnergyValue = document.createElement('div');
+    playerEnergyValue.id = 'playerEnergyValue';
+    playerEnergyValue.style.cssText = 'font-family: Monospace;' +
+        'display: inline-block';
+    playerEnergyTextDiv.appendChild(playerEnergyValue);
+
+    var playerEnergyText = document.createElement('div');
+    playerEnergyText.id = 'playerEnergyText';
+    playerEnergyText.style.cssText = 'font-size: 70%; padding-left:' +
+    '3%; display: inline-block; margin-bottom: 0px; padding-bottom: 0px;';
+    playerEnergyText.innerHTML = '% Energy';
+    playerEnergyTextDiv.appendChild(playerEnergyText);
 
     // Energy bar
     var playerEnergyDiv = document.createElement('div');
     playerEnergyDiv.id = 'playerEnergyDiv';
-    playerEnergyDiv.style.cssText ='height: 80%; bottom: 0;';
+    playerEnergyDiv.style.cssText ='height: 60%; bottom: 0;';
     container.appendChild(playerEnergyDiv);
 
     // var playerEnergyText = document.createElement('div');
     // playerEnergyText.id = 'playerEnergyText';
     // playerEnergyText.style.cssText = 'transform: translateY(+100%)
     // rotate(-90deg); display: inline-block; left:0px;
-    // position:relative;'; 
+    // position:relative;';
     // playerEnergyText.innerHTML = 'Player Health';
     // playerEnergyDiv.appendChild(playerEnergyText);
 
@@ -53,13 +72,13 @@ var PlayerInfo = function() {
     playerEnergyPercentage.id = 'playerEnergyPercentage';
     playerEnergyPercentage.style.cssText = 'width: 100%; height: 100%;' +
         'background-color: #4CAF50; display: inline-block; bottom:0;' +
-        'position: absolute; bottom:0;'; 
+        'position: absolute; bottom:0;';
     playerEnergyBar.appendChild(playerEnergyPercentage);
 
     // var playerEnergyPercentageText = document.createElement('div');
     // playerEnergyPercentageText.id = 'playerEnergyPercentageText';
     // playerEnergyPercentageText.style.cssText = 'top:50%; left:50%;
-    // transform: rotate(-90deg);'; 
+    // transform: rotate(-90deg);';
     // playerEnergyPercentageText.innerHTML = '50%';
     // playerEnergyBar.appendChild(playerEnergyPercentageText);
 
@@ -80,23 +99,31 @@ var PlayerInfo = function() {
         domElement: container,
         updateEnergy : function(newPlayerEnergy) {
             var currentEnergy = parseInt(playerEnergyPercentage.style.height);
+            var energyDecreasing = (currentEnergy > newPlayerEnergy);
+
+            playerEnergyValue.innerHTML = currentEnergy;
+
+            if (energyDecreasing){
+                playerEnergyValue.style.color = '#FF0000';
+                playerEnergyPercentage.style.backgroundColor =
+                    '#FF0000';
+            } else if (currentEnergy < newPlayerEnergy) {
+                playerEnergyValue.style.color = '#00FF00';
+            }
 
             var id = setInterval(frame, 10);
             function frame() {
                 if (currentEnergy == newPlayerEnergy) {
                     clearInterval(id);
-                } else {
-                    if (currentEnergy < newPlayerEnergy) {
-                        ++currentEnergy;
-                    } else {
-                        --currentEnergy;
-                    }
-                    playerEnergyPercentage.style.height = currentEnergy + '%';
+                    playerEnergyValue.style.color = '';
                     playerEnergyPercentage.style.backgroundColor =
-                        energyPercentageColor(currentEnergy); 
+                        energyPercentageColor(currentEnergy);
+                } else {
+                    (energyDecreasing) ? --currentEnergy : ++currentEnergy;
+                    playerEnergyPercentage.style.height = currentEnergy + '%';
+                    playerEnergyValue.innerHTML = currentEnergy;
                 }
             }
-
         },
         updateScore : function(newPlayerScore) {
             playerScoreValue.innerHTML = newPlayerScore;
