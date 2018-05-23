f80bd4ac6c0a598c3804b15e0f6869c0dd8302bc
lulivi <luislivilla@gmail.com>
2018-05-23 03:10:20 +0200

    P3: Added a new method of queuing the ammo bar updates. fixes #7     Added documentation for new methods and old ones.

--- practica_3/tanks/ammobar.js
+++ practica_3/tanks/ammobar.js
@@ -5,6 +5,12 @@ var AmmoBar = function(id = 0, ammoColor = 'red') {
     // Current ammo count
     var currentAmmo = 20;
 
+    // To queue new updates and don't
+    var updateQueue = [];
+
+    // Check if the function changeAmmo is executing
+    var updatingAmmo = false;
+
     // Bar id
     var barId = 'ammoBar' + id;
 
@@ -22,6 +28,11 @@ var AmmoBar = function(id = 0, ammoColor = 'red') {
             $('<i>').addClass('w3-cell-middle fas fa-heart')
         ).append(' ');
 
+    /**
+     * Toggle between grey and the real color of the heart
+     *
+     * @param {Number} heartIdx - Index of the heart to toggle color
+     */
     var toggleHeart = function(heartIdx) {
         return $('#' + barId)
             .children('i')
@@ -29,52 +40,121 @@ var AmmoBar = function(id = 0, ammoColor = 'red') {
             .toggleClass('w3-text-dark-gray');
     };
 
-    // Function to animate increment/decrement of the current ammo
-    var changeAmmo = function(newAmmo) {
-        var ammoDecreasing = null;
-
-        // Set the correct action (decrease/increase) and change the colours
-        // regarding the action
-        if (currentAmmo > newAmmo) {
-            ammoDecreasing = true;
-            $('#' + barId)
-                .toggleClass('w3-pale-red w3-light-grey');
-        } else {
-            ammoDecreasing = false;
-            $('#' + barId)
-                .toggleClass('w3-pale-green w3-light-grey');
-        }
-
-        // Create an interval to animate the ammo change
-        var interval = setInterval(frame, 100);
-        function frame() {
-            if (currentAmmo == newAmmo) {
-                clearInterval(interval);
-                // End of the interval. Clear green and red colours and toggle
-                // basic grey one
+    /**
+     * Animate the change of the current ammo
+     *
+     * @param {Number} lastCurrentAmmo - currentAmmo value when the function
+     * were queued
+     * @param {Number} newAmmo - New value of ammo
+     */
+    var changeAmmo = function(lastCurrentAmmo, newAmmo) {
+        updatingAmmo = true;
+        return new Promise((resolve, reject) => {
+            var ammoDecreasing = null;
+            // Set the correct action (decrease/increase) and change the
+            // colours regarding the action
+            if (lastCurrentAmmo > newAmmo) {
+                ammoDecreasing = true;
+                // Remove the basic grey color and add the new red-decreassing
+                // color
                 $('#' + barId)
-                    .removeClass('w3-pale-green w3-pale-red')
-                    .toggleClass('w3-light-grey');
+                    .toggleClass('w3-pale-red w3-light-grey');
             } else {
-                if (ammoDecreasing) {
-                    --currentAmmo;
-                    toggleHeart(currentAmmo);
+                ammoDecreasing = false;
+                // Remove the basic grey color and add the new
+                // green-increassing color
+                $('#' + barId)
+                    .toggleClass('w3-pale-green w3-light-grey');
+            }
+
+            // Create an interval to animate the ammo change
+            var interval = setInterval(frame, 100);
+            function frame() {
+                if (lastCurrentAmmo == newAmmo) {
+                    // End of the interval
+                    clearInterval(interval);
+                    // Clear green and red colours and
+                    // add basic grey one
+                    $('#' + barId)
+                        .removeClass('w3-pale-green w3-pale-red')
+                        .addClass('w3-light-grey');
+                    // Finish the promise
+                    resolve();
                 } else {
-                    toggleHeart(currentAmmo);
-                    ++currentAmmo;
+                    if (ammoDecreasing) {
+                        --lastCurrentAmmo;
+                        toggleHeart(lastCurrentAmmo);
+                    } else {
+                        toggleHeart(lastCurrentAmmo);
+                        ++lastCurrentAmmo;
+                    }
                 }
             }
-        }
+        });
+    };
+
+    /**
+     * Create a promise to turn updatingAmmo off after the changeAmmo function
+     * execution
+     *
+     * @param {Number} lastCurrentAmmo - CurrentAmmo value when the function
+     * were queued
+     * @param {Number} newAmmo - New value of ammo
+     */
+    var promiseChangeAmmo = function(lastCurrentAmmo, newAmmo) {
+        changeAmmo(lastCurrentAmmo, newAmmo).then(function(){
+            updatingAmmo = false;
+        });
+    };
+
+    /**
+     * Function wrapping code.
+     *
+     * @param {Object} fn - Reference to a function for queuing
+     * @param {context} context - Function context
+     * @param {Array<Object>} params - Array of parameters to pass the function
+     */
+    var wrapFunction = function(fn, context, params) {
+        return function() {
+            fn.apply(context, params);
+        };
+    };
+
+    /**
+     * Queue a new update.
+     *
+     * @param {Number} lastCurrentAmmo - CurrentAmmo value when the function
+     * were queued
+     * @param {Number} newAmmo - New value of ammo
+     */
+    var queueAmmoUpdate = function(lastCurrentAmmo, newAmmo) {
+        updateQueue.push(
+            wrapFunction(promiseChangeAmmo, this, [lastCurrentAmmo, newAmmo])
+        );
     };
 
     return {
         id: barId,
-        currentAmmo: currentAmmo,
+        getVars: function() {
+            return {
+                currentAmmo: currentAmmo,
+                updatingAmmo: updatingAmmo,
+                updateQueue: updateQueue,
+            };
+        },
         color: ammoColor,
         domElement: bar,
         updateAmmo: function(newAmmo) {
-            if (0 <= newAmmo && newAmmo <= 20 && newAmmo != currentAmmo)
-                changeAmmo(newAmmo);
+            // Execute one ammo update of the queue
+            if (!updatingAmmo && updateQueue.length > 0)
+                (updateQueue.shift())();
+            // Queue a new ammo update
+            if (0 <= newAmmo && newAmmo <= 20 && newAmmo != currentAmmo) {
+                // Queue a new update with currentAmmo and the newAmmo
+                queueAmmoUpdate(currentAmmo, newAmmo);
+                // Change the currentAmmo var for futures updates
+                currentAmmo = newAmmo;
+            }
         },
     };
 };
